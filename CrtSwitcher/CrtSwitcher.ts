9a259000d4d9f7010f9fe54f6c9dac527b583db8
onadrog <sebastien.gordano@gmail.com>
2023-05-16 16:35:31 +0200

    Bump: license

--- CrtSwitcher/CrtSwitcher.ts
+++ CrtSwitcher/CrtSwitcher.ts
@@ -1,52 +1,77 @@
+/**
+ * @licence
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright 2023 Sébastien Gordano
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 class CrtSwitcher extends HTMLElement {
-    root: DOMTokenList;
-    svgPath: SVGPathElement;
-    input: HTMLInputElement;
-    fill: string;
-    constructor() {
-        super();
-        this.root = document.documentElement.classList;
-        this.input = this.querySelector("input") as HTMLInputElement;
-        this.svgPath = this.querySelector(
-            "svg > path.crt-screen"
-        ) as SVGPathElement;
-        this.fill = "none";
-    }
+	root: DOMTokenList;
+	svgPath: SVGPathElement;
+	input: HTMLInputElement;
+	fill: string;
+	constructor() {
+		super();
+		this.root = document.documentElement.classList;
+		this.input = this.querySelector("input") as HTMLInputElement;
+		this.svgPath = this.querySelector(
+			"svg > path.crt-screen",
+		) as SVGPathElement;
+		this.fill = "none";
+	}
 
-    connectedCallback() {
-        this.getSessionStorage();
-        this.fill === "currentColor"
-            ? (this.input.checked = true)
-            : (this.input.checked = false);
-        this.input.addEventListener("change", (e) => this.handleToggle(e));
-    }
+	connectedCallback() {
+		this.getSessionStorage();
+		this.fill === "currentColor"
+			? (this.input.checked = true)
+			: (this.input.checked = false);
+		this.input.addEventListener("change", (e) => this.handleToggle(e));
+	}
 
-    getSessionStorage(): void {
-        if (sessionStorage.getItem("fill")) {
-            this.fill = sessionStorage.getItem("fill") as string;
-        }
-        this.changeEffect();
-    }
+	getSessionStorage(): void {
+		if (sessionStorage.getItem("fill")) {
+			this.fill = sessionStorage.getItem("fill") as string;
+		}
+		this.changeEffect();
+	}
 
-    private handleToggle(e: Event) {
-        const target = e.target as HTMLInputElement;
-        if (target.checked) {
-            this.fill = "currentColor";
-        } else {
-            this.fill = "none";
-        }
-        this.changeEffect();
-    }
+	private handleToggle(e: Event) {
+		const target = e.target as HTMLInputElement;
+		if (target.checked) {
+			this.fill = "currentColor";
+		} else {
+			this.fill = "none";
+		}
+		this.changeEffect();
+	}
 
-    private changeEffect() {
-        if (this.fill === "none") {
-            this.root.remove("crt");
-        } else {
-            this.root.add("crt");
-        }
-        sessionStorage.setItem("fill", this.fill);
-        this.svgPath.setAttribute("fill", this.fill);
-    }
+	private changeEffect() {
+		if (this.fill === "none") {
+			this.root.remove("crt");
+		} else {
+			this.root.add("crt");
+		}
+		sessionStorage.setItem("fill", this.fill);
+		this.svgPath.setAttribute("fill", this.fill);
+	}
 }
 
 customElements.define("crt-switcher", CrtSwitcher);
--- themeSwitcher/ThemeSwitcher.ts
+++ themeSwitcher/ThemeSwitcher.ts
@@ -1,48 +1,73 @@
+/**
+ * @licence
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright 2023 Sébastien Gordano
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 class ThemeSwitcher extends HTMLElement {
-    root: DOMTokenList
-    theme: string
-    inputs: NodeListOf<HTMLInputElement>
-    constructor() {
-        super();
-        this.root = document.documentElement.classList;
-        this.theme = "";
-        this.inputs = this.querySelectorAll("input[name='theme-toggle']");
-    }
+	root: DOMTokenList;
+	theme: string;
+	inputs: NodeListOf<HTMLInputElement>;
+	constructor() {
+		super();
+		this.root = document.documentElement.classList;
+		this.theme = "";
+		this.inputs = this.querySelectorAll("input[name='theme-toggle']");
+	}
 
-    connectedCallback() {
-        this.getSessionStorage();
-        (this.querySelector(`input[value=${this.theme}]`) as HTMLInputElement).checked = true;
-        this.inputs.forEach((i) => i.addEventListener("change", (e) => this.handleToggle(e)));
-    }
+	connectedCallback() {
+		this.getSessionStorage();
+		(
+			this.querySelector(`input[value=${this.theme}]`) as HTMLInputElement
+		).checked = true;
+		this.inputs.forEach((i) =>
+			i.addEventListener("change", (e) => this.handleToggle(e)),
+		);
+	}
 
-    getSessionStorage(): void {
+	getSessionStorage(): void {
+		if (sessionStorage.getItem("theme")) {
+			this.theme = sessionStorage.getItem("theme") as string;
+		} else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+			this.theme = "dark";
+		} else {
+			this.theme = "light";
+		}
+		this.changeTheme();
+	}
 
-        if (sessionStorage.getItem("theme")) {
-            this.theme = sessionStorage.getItem("theme") as string;
-        }
-        else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-            this.theme = "dark";
-        } else {
-            this.theme = "light";
-        }
-        this.changeTheme();
-    }
+	handleToggle(e: Event): void {
+		const target = e.target as HTMLInputElement;
+		this.theme = target.value;
+		sessionStorage.setItem("theme", this.theme);
+		this.changeTheme();
+	}
 
-    handleToggle(e: Event): void {
-        const target = e.target as HTMLInputElement;
-        this.theme = target.value;
-        sessionStorage.setItem("theme", this.theme);
-        this.changeTheme();
-    }
-
-    changeTheme() {
-
-        if (this.theme === "light") {
-            this.root.remove("dark");
-        } else {
-            this.root.add("dark");
-        }
-    }
-
+	changeTheme() {
+		if (this.theme === "light") {
+			this.root.remove("dark");
+		} else {
+			this.root.add("dark");
+		}
+	}
 }
-customElements.define('theme-switcher', ThemeSwitcher);
+customElements.define("theme-switcher", ThemeSwitcher);
